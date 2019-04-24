de8f5124096216176f80468bc6976461e695ed78
jnamgung <namgung.john@gmail.com>
2019-04-24 14:51:14 -0400

    Worked on product page

---
+++ js/single-product.js
@@ -0,0 +1,39 @@
+function react_navy_blue() {
+    document.getElementById('product_img_1').src = 'images/product_page/navy_blue_1.jpg'
+    document.getElementById('product_img_2').src = 'images/product_page/navy_blue_2.jpg'
+    document.getElementById('product_img_3').src = 'images/product_page/navy_blue_3.jpg'
+    document.getElementById('product_img_4').src = 'images/product_page/navy_blue_4.jpg'
+    document.getElementById('product_img_5').src = 'images/product_page/navy_blue_5.jpg'
+}
+
+function react_graphite() {
+    document.getElementById('product_img_1').src = 'images/product_page/graphite_1.jpg'
+    document.getElementById('product_img_2').src = 'images/product_page/graphite_2.jpg'
+    document.getElementById('product_img_3').src = 'images/product_page/graphite_3.jpg'
+    document.getElementById('product_img_4').src = 'images/product_page/graphite_4.jpg'
+    document.getElementById('product_img_5').src = 'images/product_page/graphite_5.jpg'
+}
+
+function react_chocolate_brown() {
+    document.getElementById('product_img_1').src = 'images/product_page/chocolate_brown_1.jpg'
+    document.getElementById('product_img_2').src = 'images/product_page/chocolate_brown_2.jpg'
+    document.getElementById('product_img_3').src = 'images/product_page/chocolate_brown_3.jpg'
+    document.getElementById('product_img_4').src = 'images/product_page/chocolate_brown_4.jpg'
+    document.getElementById('product_img_5').src = 'images/product_page/chocolate_brown_5.jpg'
+}
+
+function react_tuxedo_black() {
+    document.getElementById('product_img_1').src = 'images/product_page/tuxedo_black_1.jpg'
+    document.getElementById('product_img_2').src = 'images/product_page/tuxedo_black_2.jpg'
+    document.getElementById('product_img_3').src = 'images/product_page/tuxedo_black_3.jpg'
+    document.getElementById('product_img_4').src = 'images/product_page/tuxedo_black_4.jpg'
+    document.getElementById('product_img_5').src = 'images/product_page/tuxedo_black_5.jpg'
+}
+
+function react_charcoal() {
+    document.getElementById('product_img_1').src = 'images/product_page/charcoal_1.jpg'
+    document.getElementById('product_img_2').src = 'images/product_page/charcoal_2.jpg'
+    document.getElementById('product_img_3').src = 'images/product_page/charcoal_3.jpg'
+    document.getElementById('product_img_4').src = 'images/product_page/charcoal_1.jpg'
+    document.getElementById('product_img_5').src = 'images/product_page/charcoal_2.jpg'
+}
